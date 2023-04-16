12da29db555083d1f0d15e1a4cb87180d247f937
Ivan <my_world1105@mail.ru>
2023-04-16 16:09:54 +0300

    add 1 step of validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,16 +10,15 @@ class ContactController extends Controller {
     }
 
     public function submit(Request $request){
+        // dd($request->input());
         $validation = $request->validate([
-            'fio' => 'required|alpha',
-            'date' => 'required',
-            'tel' => 'required',
+            'fio' => 'required|regex:/[а-яА-Я]{1,}\s[а-яА-ЯёЁ]{1,}\s[а-яА-Я]{1,}/',
+            'date' => 'required|regex:/\d+\/\d+\/\d{4}/',
+            'tel' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
             'gender' => 'required',
-            'email' => 'required',
+            'email' => 'required|email',
             'age' => 'required',
         ]);
-        echo $validation;
-        dd($request->input());
-        
+        // echo $validation;
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,99 @@
 @extends('layout.base')
 
+@section('links')
+    <link rel="stylesheet" type="text/css" href="{{ url('css/modal.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ url('css/tip.css') }}" >
+@endsection
+
 @section('title')
     Контакты
 @endsection
 
 @section('body')
-    <h1>Контакты</h1>
+    <!-- <div id='modal' class="modal">
+        <div class="modal-window">
+            <div id="modal-content">
+                Вы уверены, что хотите отправить письмо?
+            <div id="downPanel">
+                <button id="prev" class="btn-prev">Нет</button>
+                <button id="next" class="btn-next">Да</button>
+            </div>
+            </div><br>
+            <button id='btn-close' class="btn-close">X</button>
+        </div>
+        <div class="overlay"></div>
+    </div> -->
+   
+
+    <!-- action="mailto:my_world1105@mail.ru?subject=FeedBack" -->
+    <!-- /contact/submit -->
+    @if($errors->any())
+    <div class="alert">
+        <ul>
+            @foreach($errors->all() as $e)
+            <li>{{ $e }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form id="form" action="{{ route('contact-form') }}" method="POST">
+        @csrf
+        <fieldset>
+            <legend>Отправить нам письмо</legend>
+
+            <!-- Фамилия Имя Отчество -->
+            <div class="box">
+                <label for="fio">Ваше ФИО:</label>
+                <input id="fio" type="text" name="fio" placeholder="Коновалов Иван Викторович" >
+            </div>
+
+            <!-- Дата рождения -->
+            <div class="box">
+                <label for="my_date">Дата рождения:</label>
+                <input id="my_date" type="text" name="date" placeholder="ММ/ДД/ГГГГ">
+            </div>
+
+            <!-- Номер телефона -->
+            <div class="box">
+                <label for="tel">Телефон:</label>
+                <input id="tel" type="tel" name="tel" placeholder="+78005553535"> 
+            </div>
+                
+            <!-- Пол -->
+            <div class="box">
+                <label for="gender">Пол:</label>
+                <input type="radio" id="women" name="gender" value="Женский" checked>
+                <label for="women">Женщина</label>
+                <input type="radio" id="man" name="gender" value="Мужской">
+                <label for="man">Мужчина</label>
+            </div>
+
+            <!-- Почта -->
+            <div class="box">
+                <label for="email">Email для связи:</label>
+                <input id="email" type="email" name="email" placeholder="Адрес электронной почты">
+            </div>
+
+            <!-- Возраст -->
+            <div class="box">
+                <label for="age">Возраст:</label>
+                <select name="age">
+                    <option value="0-14">меньше 14</option>
+                    <option value="14-18">от 14 до 18</option>
+                    <option value="18-27">от 18 до 27</option>
+                    <option value="27-45">от 27 до 45</option>
+                    <option value="45-69">от 45 до 69</option>
+                    <option value="69-100">от 69 и больше</option>
+                </select>
+            </div>
+
+            <!-- Кнопки управления -->
+            <div class="box">
+                <input id="send" type="submit" value="Отправить">
+                <input type="reset" value="Отчистить форму"> <br>
+            </div>
+        </fieldset>
+    </form>
+   
 @endsection
