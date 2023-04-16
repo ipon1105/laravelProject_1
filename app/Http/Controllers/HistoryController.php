09ed4856c2d2d17dfd66a8d82a03644d41e3bb34
Ivan <my_world1105@mail.ru>
2023-04-16 12:10:11 +0300

    All bad

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+
+class HistoryController extends Controller {
+    public function show() {
+        return view('history');
+    }
+}
---
+++ app/Http/Controllers/HobbyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+
+class HobbyController extends Controller {
+    public function show() {
+        return view('hobby');
+    }
+}
