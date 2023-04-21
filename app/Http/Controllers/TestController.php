d3bc7152e466613ab8227aae3d58251e05733a8c
Ivan <my_world1105@mail.ru>
2023-04-21 10:04:34 +0300

    ALL GOOD

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,7 +13,18 @@ class TestController extends Controller {
 
     public function submit(TestRequest $request){
         $request->validated();
+        
+        $i = 0;
+        
+        if ($request->input('check') != null &&
+            count($request->input('check')) == 2 &&
+            strcmp($request->input('check')[0], 'check_1') == 0 &&
+            strcmp($request->input('check')[1], 'check_2') == 0) $i = $i + 1;
+        if (strcmp($request->input('select'), 'select_2') == 0) $i = $i + 1;
+        if (strcmp($request->input('input_1'), 'Треугольной') == 0) $i = $i + 1;
 
-        return redirect()->route('test')->with('success', 'Ваш тест отправлен');
+        return redirect()->route('test')->with('success', 'Ваш тест отправлен. Результат: '.$i.'/3');
     }
 }
+/*
+Рализовать всю проверку с помощью встроенных правил валидации Laravel
