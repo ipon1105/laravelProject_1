ca80b1f0c561210e60750b500bcbecc84ec6b6fe
Ivan <my_world1105@mail.ru>
2023-04-30 16:49:01 +0300

    fill db data from form

--- app/Http/Controllers/GuessController.php
+++ app/Http/Controllers/GuessController.php
@@ -14,7 +14,11 @@ class GuessController extends Controller
     public function submit(Request $request){
         $feedback = new Feedback();
         
-        $feedback->name = $request->input('i');
+        $feedback->name = $request->input('name');
+        $feedback->surname = $request->input('surname');
+        $feedback->patronymic = $request->input('patronymic');
+        $feedback->email = $request->input('email');
+        $feedback->msg = $request->input('msg');
         // $request->validated();
         
         // return redirect()->route('guess')->with('success', 'Ваш тест отправлен.');
