bf4f96824afde026c4a220b3ee7b65cbe16655b6
Ivan <my_world1105@mail.ru>
2023-05-17 20:42:40 +0300

    good

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -121,9 +121,29 @@ class BlogController extends Controller
                 $note->content = $temp[1];
                 $note->created_at = $temp[3];
                 $note->updated_at = $temp[3];
+
+                // Путь до файла если он есть
+                if ($temp[4] == null || $temp[4] == "" || $temp[4] == '')
+                    $note->filename = null;
+                else{
+                    $image = file_get_contents($temp[4]);
+                    $filename = 'uploads/'.sha1(time()).'.'.pathinfo($temp[4], PATHINFO_EXTENSION);
+                    $note->filename = $filename;
+
+                    file_put_contents(storage_path('app/public/'.$filename), $image);
+
+                    // file_put_contents("/var/www/laravelProject_1/storage/app/public/uploads/".sha1(time()).'.png', $image );
+                    // ->store('uploads', 'public');
+                    // file_put_contents();
+                    //  = $request->file($temp[4])->store();
+                    // Storage::disk('public')->storeFile
+                }
+
                 $note->save();
             }
         }
+        // $data = Note::orderBy('created_at', 'desc')->paginate($perPage, ['*'], 'page', $index);
+
         return redirect()->route('blog')->with('success', 'Записи загружены');
     }
 
