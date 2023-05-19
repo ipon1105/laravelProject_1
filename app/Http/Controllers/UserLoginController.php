85a2662f586526dd95ea7a3e213b68d5657372a3
Ivan <my_world1105@mail.ru>
2023-05-19 11:04:59 +0300

    good one

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -3,22 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
+use App\Models\Statistic;
 use Auth;
 use Illuminate\Http\Request;
+use Redirect;
+use Session;
+use URL;
 
 class UserLoginController extends Controller
 {
-    
-    public function show(){
+    public function show(Request $request){
+        // dd($request->session()->previousUrl());
+        if (!Session::get('back')){
+            Session::put('back', $request->session()->previousUrl());
+        }
+
         return view('user-login');
     }
 
     public function submit(UserLoginRequest $request) {
+        // dd($request->session()->previousUrl());
+        // dd(session());
         $credentials = $request->getCredentials();
         
         if (!Auth::attempt($credentials))
             return redirect()->route('user-login')->withErrors(['email' => 'Не верный логин или пароль.']);
-        
-        return redirect()->route('test');
+
+        return Redirect::to(Session::get('back'));
     }
+
 }
