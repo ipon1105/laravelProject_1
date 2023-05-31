824166321f8f251c7cb0e1a779bb2183b8b3ea17
Ivan Konovalov <my_world1105@mail.ru>
2023-05-31 21:32:10 +0300

    Добавил кнопку навигации для администратора

--- resources/views/inc/bheader.blade.php
+++ resources/views/inc/bheader.blade.php
@@ -3,6 +3,7 @@
     <h1 class="bold topp left" > $ ./Hacker-Blog.sh "@yield('title')"</h1>
       
     <a href="{{route('blog')}}" class="button leftmar">Библиотека записей</a>
-    <a href="https://github.com/ipon1105/laravelProject_1" class="button ">View on Github</a>
+    <a href="https://github.com/ipon1105/laravelProject_1" class="button">View on Github</a>
+    <a href="{{route('admin')}}" class="button">Admin</a>
     <hr class="hr"> <br>
 </header>
